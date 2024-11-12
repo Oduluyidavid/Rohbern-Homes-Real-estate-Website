7335e050a1e21e73bda82fb4e1ee2f503afda235
unknown <133164451+Oduluyidavid@users.noreply.github.com>
2024-11-13 02:28:46 +0800

    today

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -5,7 +5,32 @@ import "aos/dist/aos.css";
 import emailjs from "@emailjs/browser";
 
 const Contact = () => {
-  
+  const form = useRef();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    emailjs
+      .sendForm(
+        "service_8m46np8",
+        "template_tfb16yq",
+        form.current,
+        "rULTBWkjdEMpYeEDU"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setIsLoading(false);
+          alert("Email Sent");
+        },
+        (error) => {
+          setIsLoading(false);
+          console.log(error.text);
+        }
+      );
+  };
 
   useEffect(() => {
     Aos.init({
@@ -18,8 +43,6 @@ const Contact = () => {
 
   const { darkMode, toggleDarkMode } = useDarkMode();
 
-
-
   return (
     <div
       className={`${darkMode ? "dark bg-black" : "light bg-transparent"} pb-20`}
@@ -29,23 +52,54 @@ const Contact = () => {
           darkMode ? "dark bg-gray-800" : "light bg-red-100"
         } lg:w-[95%] w-ful h-fit m-auto 
       rounded-xl grid lg:grid-cols-2  justify-center items-center lg:px-36 px-6 py-20 gap-10`}
-      id="contact"
+        id="contact"
       >
         <div
           data-aos="zoom-in"
           className="bg-white dark:bg-black p-10 flex flex-col 
-         justify-center items-start gap-4 rounded-xl w-full" >
-          <h1 className='text-2xl text-black font-semibold dark:text-white'>Send us a message today</h1>
-          <input type='text' placeholder='Enter your full name here' 
-          className='w-full px-6 py-3 border-2 border-gray-200 rounded-xl p-5' />
-          <input type='email' placeholder='Enter your valid email' 
-          className='w-full px-6 py-3 border-2 border-gray-200 rounded-xl p-5' />
-          <input type='number' placeholder='Enter your vaild mobile number' 
-          className='w-full px-6 py-3 border-2 border-gray-200 rounded-xl p-5' />
-          <textarea name='' id='' cols='30'rows='5' placeholder='Enter your message here...
-          'className='w-full px-6 py-3 border-2 border-gray-200 rounded-xl p-5'></textarea>
-          <button className='bg-red-600 w-full text-md px-8 py-3 text-white font-semibold 
-          rounded-xl hover:bg-black dark:hover:bg-red-700 cursor-pointer p-5'>SEND MAIL</button>
+         justify-center items-start gap-4 rounded-xl w-full"
+        >
+          <form className="contactForm flex flex-col gap-3" ref={form} onSubmit={sendEmail}>
+            <h1 className="text-2xl text-black font-semibold dark:text-white space-x-3">
+              Send us a message today
+            </h1>
+            <input
+              type="text"
+              placeholder="Enter your full name here"
+              className="w-full px-6 py-3 border-2 border-gray-200 rounded-xl p-5 space-x-3"
+            />
+            <input
+              type="email"
+              placeholder="Enter your valid email"
+              className="w-full px-6 py-3 border-2 border-gray-200 rounded-xl p-5"
+            />
+            <input
+              type="number"
+              placeholder="Enter your vaild mobile number"
+              className="w-full px-6 py-3 border-2 border-gray-200 rounded-xl p-5"
+            />
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="5"
+              placeholder="Enter your message here...
+          "
+              className="w-full px-6 py-3 border-2 border-gray-200 rounded-xl p-5 "
+            ></textarea>
+            <button
+              type="Sumbit"
+              disabled={isLoading}
+              value="send"
+              onClick={(e) => {
+                sendEmail(e);
+              }}
+              className="btn px-14 py-4 shadow-sm bg-red-600 w-full text-md text-white font-semibold 
+               rounded-xl hover:bg-black dark:hover:bg-red-700 cursor-pointer p-5"
+            >
+              {isLoading ? "loading..." : "Send Mail"}
+            </button>
+          </form>
         </div>
       </section>
     </div>
